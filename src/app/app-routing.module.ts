66ff74c76806bb2dee7acc54e909a10d13dc3501
Pavan Mundada <pavanpmundada5793@gmail.com>
2023-06-09 10:43:40 +0530

    removed add-emp component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,9 @@ import { EmplyoeeDashboardComponent } from './emplyoee-dashboard/emplyoee-dashbo
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
-import { AddEMplyoeeComponent } from './add-emplyoee/add-emplyoee.component';
 
 const routes: Routes = [
   { path: '', component: EmplyoeeDashboardComponent },
-  { path: 'add-emp', component: AddEMplyoeeComponent },
 ];
 
 @NgModule({
